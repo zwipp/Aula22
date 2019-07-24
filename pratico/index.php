525caf167a822a3919d717fc9bae245e281b7bac
zwipp <nuneszwipp@hotmail.com>
2019-07-24 11:48:51 -0300

    adicionando contatos

--- pratico/index.php
+++ pratico/index.php
@@ -4,12 +4,33 @@
 
     //carregando o conteudo do arquivo para uma variavel
     
-
     function getContatos(){ //coloca em uma função pois será usados diversas vezes
 
         $json = file_get_contents(J); //pegando o conteudo do arquivo e jogando em uma variavel
         $contatos = json_decode($json,true); //transformando o arquivo em um array
         return $contatos;
+    }
+
+    //função que add contato ao json
+    function addContato($nome,$email){
+        //carregando os contatos
+        $contatos = getContatos();
+
+        //add um novo contato ao array de contatos
+        $contatos[] = [
+            'nome'=> $nome,
+            'email' => $email
+        ];
+        //transformar o array contatos em uma string json
+        $json = json_encode($contatos);
+
+        //salvar a string json no arquivo
+        file_put_contents(J,$json);
+    }
+
+    if($_POST){ //ver se tem conteudo
+        //adicionar contato ao arquivo json
+        addContato($_POST['nome'],$_POST['email']);
     }
 
     $contatos = getContatos();
@@ -37,8 +58,8 @@
     </ul>
     
     <form action="index.php" method="post">
-        <input type="text" name="nome" id="nome" placeholder="digite um nome">
-        <input type="text" name="email" id="email" placeholder="digite um email">
+        <input required type="text" name="nome" id="nome" placeholder="digite um nome">
+        <input required type="email" name="email" id="email" placeholder="digite um email">
         <button type="submit">Salvar</button>
     </form>
 
